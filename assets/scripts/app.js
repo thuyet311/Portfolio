409026c8cc71c47a685e0b9bfc77db22e38404d6
thuyet311 <58371814+thuyet311@users.noreply.github.com>
2020-05-26 17:15:32 +0700

    ver 1

---
+++ assets/scripts/app.js
@@ -0,0 +1,82 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "football",
+				weight: 9
+			}, {
+				text: "gym",
+				weight: 10
+			}, {
+				text: "Materials engineering",
+				weight: 9
+			}, {
+				text: "Solidworks",
+				weight: 8
+			}, {
+				text: "ambassador",
+				weight: 8.5
+			}, {
+				text: "PhD",
+				weight: 12
+			}, {
+				text: "CAD",
+				weight: 8.3
+			}, 
+			 {
+				text: "Sheffield Hallam",
+				weight: 8
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am doing a PhD", "also teach Maths to people.", "love beer and gym."
+			],
+			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+			stringsElement: null,
+			// typing speed
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#fca311"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
